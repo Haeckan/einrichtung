9bd335f3f70daa659c814aba77fc9ab9e6c30216
Haeckan <31367954+Haeckan@users.noreply.github.com>
2025-05-25 15:07:07 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -70,60 +70,6 @@ for PLUGIN in "${PLUGINS[@]}"; do
     esac
 done
 
-# Plugins in .zshrc aktivieren (robust und doppelfrei)
-WANTED_PLUGINS=(zsh-autosuggestions zsh-syntax-highlighting tmux cp zsh-interactive-cd fzf)
-
-if [ -f "$HOME/.zshrc" ]; then
-    echo "[+] Passe .zshrc Plugin-Zeile an"
-    EXISTING=$(grep -oP '^plugins=\(.*?\)' "$HOME/.zshrc" | sed -E 's/^plugins=\((.*)\)/\1/' | tr -d "\n")
-    IFS=' ' read -r -a CURRENT_PLUGINS <<< "$EXISTING"
-    PLUGIN_SET=()
-    for PLUGIN in "${CURRENT_PLUGINS[@]}" "${WANTED_PLUGINS[@]}"; do
-        [[ " ${PLUGIN_SET[*]} " == *" $PLUGIN "* ]] || PLUGIN_SET+=("$PLUGIN")
-    done
-    NEW_LINE="plugins=(${PLUGIN_SET[*]})"
-    sed -i "s/^plugins=.*/$NEW_LINE/" "$HOME/.zshrc"
-else
-    echo "plugins=(${WANTED_PLUGINS[*]})" >> "$HOME/.zshrc"
-fi
-
-# Zusätzliche Konfigurationen in .zshrc vor und nach source $ZSH/oh-my-zsh.sh
-if grep -q 'source \$ZSH/oh-my-zsh.sh' "$HOME/.zshrc"; then
-    PRE_LINES=(
-        'ENABLE_CORRECTION="true"'
-        'zstyle '\''':omz:update'\'' mode auto'
-        'ZSH_TMUX_AUTOSTART="true"'
-        'ZSH_TMUX_UNICODE="true"'
-        'ZSH_TMUX_FIXTERM_WITH_256COLOR="true"'
-    )
-    for LINE in "${PRE_LINES[@]}"; do
-        grep -qxF "$LINE" "$HOME/.zshrc" || sed -i "/source \$ZSH\/oh-my-zsh.sh/i $LINE" "$HOME/.zshrc"
-    done
-
-    if ! grep -q 'source <(fzf --zsh)' "$HOME/.zshrc"; then
-        sed -i "/source \$ZSH\/oh-my-zsh.sh/a source <(fzf --zsh)" "$HOME/.zshrc"
-    fi
-else
-    {
-        echo 'ENABLE_CORRECTION="true"'
-        echo 'zstyle '\''':omz:update'\'' mode auto'
-        echo 'ZSH_TMUX_AUTOSTART="true"'
-        echo 'ZSH_TMUX_UNICODE="true"'
-        echo 'source $ZSH/oh-my-zsh.sh'
-        echo 'source <(fzf --zsh)'
-    } >> "$HOME/.zshrc"
-fi
-
-# Editor und fzf Optionen ans Ende der .zshrc
-EDITOR_LINES=(
-    "export EDITOR='nano'"
-    "export FZF_DEFAULT_OPTS='--height 40% --tmux 60% --layout reverse --border top'"
-    "export TERM=xterm-256color"
-)
-for LINE in "${EDITOR_LINES[@]}"; do
-    grep -qxF "$LINE" "$HOME/.zshrc" || echo "$LINE" >> "$HOME/.zshrc"
-fi
-
 # FZF über GitHub installieren
 if [ ! -d "$HOME/.fzf" ]; then
     echo "[+] Installiere fzf von GitHub"
@@ -133,25 +79,13 @@ else
     echo "[i] fzf ist bereits installiert."
 fi
 
-# 5. Tmux Plugin Manager installieren und einrichten
+# 5. Tmux Plugin Manager installieren
 TPM_DIR="$HOME/.tmux/plugins/tpm"
 if [ ! -d "$TPM_DIR" ]; then
     echo "[+] Installiere Tmux Plugin Manager"
     git clone https://github.com/tmux-plugins/tpm "$TPM_DIR"
 else
     echo "[i] TPM ist bereits installiert."
-fi
-
-# Beispielhafte .tmux.conf hinzufügen
-if [ ! -f "$HOME/.tmux.conf" ]; then
-    echo "[+] Erstelle Beispiel ~/.tmux.conf"
-    cat <<EOF > "$HOME/.tmux.conf"
-set -g mouse on
-set -g @plugin 'tmux-plugins/tpm'
-set -g @plugin 'tmux-plugins/tmux-sensible'
-
-run '~/.tmux/plugins/tpm/tpm'
-EOF
 fi
 
 # 6. System aktualisieren und neustarten
